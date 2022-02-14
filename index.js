969328f8b3e947a1ec7672649050a1c2689dcfd4
Yuri Mayk <yuri.mayk@gmail.com>
2022-02-14 13:03:02 -0300

    it was Developed a middleware simplifying the structure of existentes routes.

--- index.js
+++ index.js
@@ -10,58 +10,75 @@ app.use(express.json())
 //Below is the array where the orders will be located
 const orders = []
 
+// Here starts the middlewares.
+
+const checkIdUser = (request, response, next) => {
+    const { id } = request.params
+
+    const index = orders.findIndex(order => order.id === id)
+
+    if (index < 0) {
+        return response.status(404).json({ error: "Order not found." })
+    }
+
+    request.id = id
+    request.orderIndex = index
+
+    next()
+
+}
+
+
+// Here ends the middlewares.
+
 // Here start the routes.
 
-app.post("/order", (require, response) => {
-    const { order, clientName, price } = require.body
-    const id = (uuid.v4())
-
-    const newOrder = { id, order, clientName, price, status: "Em preparação" }
+app.post("/order", (request, response) => {
+    const { order, clientName, price } = request.body
+    
+    const newOrder = { id: uuid.v4(), order, clientName, price, status: "Em preparação" }
     orders.push(newOrder)
 
-    response.status(201).json(newOrder)
+    return response.status(201).json(newOrder)
 
 })
 
-app.get("/order", (require, response) => {
-    response.json({ orders })
+app.get("/order", (request, response) => {
+    return response.json(orders)
 })
 
-app.put("/order/:id", (require, response) => {
-    const { order, clientName, price } = require.body
-    const { id } = require.params
-    const index = orders.findIndex(user => user.id === id)
+app.put("/order/:id", checkIdUser, (request, response) => {
+    const { order, clientName, price } = request.body
+    const id = request.id
+    const index = request.orderIndex
     const status = orders[index].status
 
     const orderUpdated = { id, order, clientName, price, status }
 
     orders[index] = orderUpdated
 
-    response.json(orderUpdated)
+    return response.json(orderUpdated)
 
 })
 
-app.delete("/order/:id", (require, response) => {
-    const { id } = require.params
-    const index = orders.findIndex(order => order.id === id)
+app.delete("/order/:id", checkIdUser, (request, response) => {
+    const index = request.orderIndex
     orders.splice(index, 1)
 
-    response.status(204).json()
+    return response.status(204).json()
 })
 
-app.get("/order/:id", (require, response) => {
-    const { id } = require.params
-    const index = orders.findIndex(order => order.id === id)
-    response.json(orders[index])
+app.get("/order/:id", checkIdUser, (request, response) => {
+    const index = request.orderIndex
+    return response.json(orders[index])
 })
 
-app.patch("/order/:id", (require, response) => {
-    const { id } = require.params
-    const index = orders.findIndex(order => order.id === id)
+app.patch("/order/:id", checkIdUser, (request, response) => {
+    const index = request.orderIndex
 
     orders[index].status = "pronto"
 
-    response.json()
+    return response.json(orders[index])
 
 })
 
